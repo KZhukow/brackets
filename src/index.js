a8d42ed911896866666afcc5f6731b1cde84a910
KZhukow <kosty.zhukov.2011@gmail.com>
2024-08-11 15:04:29 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open = bracketsConfig.map((it) => it[0]);
+  const close = bracketsConfig.map((it) => it[1]);
+
+  const arr = str.split('');
+  const stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    const openIndex = open.indexOf(arr[i]);
+    const closeIndex = close.indexOf(arr[i]);
+    if (closeIndex !== -1 && closeIndex !== stack.pop()) return false;
+    if (openIndex !== -1) arr.push(openIndex);
+  }
+  return true;
 }
